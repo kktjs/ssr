f6c1c333e07a124c83c85edb51962c815ebc9b2f
jaywcjlove <398188662@qq.com>
2018-12-21 14:52:44 +0800

    example(stylus): Update @kkt/plugin-stylus dependencies.

---
+++ example/stylus/src/app/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import styles from './App.module.styl';
+import './App.css';
+import './App.styl';
+
+export default class App extends Component {
+  render() {
+    console.log('styles.red:', styles.red);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <span className={styles.red}>KKT</span>
+        </header>
+      </div>
+    );
+  }
+}
--- example/stylus/src/client.js
+++ example/stylus/src/client.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { hydrate } from 'react-dom';
-import App from './App';
+import App from './app/App';
 
 hydrate(<App />, document.getElementById('root'));
 
