36a986a510dda66ad0ad043dfc9de088414ff324
jaywcjlove <398188662@qq.com>
2018-12-21 13:06:19 +0800

    example: Update less example.

--- example/less/src/App.js
+++ example/less/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import logo from './logo.svg';
 import styles from './App.module.less';
 import './App.css';
 import './App.less';
 
-const App = () => (
-  <div>
-    Welcome <span className="blue">to</span> <span className={styles.red}>KKT</span>.
-    <br />
-  </div>
-);
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <span className={styles.red}>Edit</span> <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
 
 export default App;
