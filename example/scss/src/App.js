6198a27d8687f52fe450aaaecc5b38ae81f8d19f
jaywcjlove <398188662@qq.com>
2018-12-21 13:14:14 +0800

    example(scss): Update @kkt/plugin-scss dependencies.

--- example/scss/src/App.js
+++ example/scss/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import logo from './logo.svg';
 import styles from './App.module.scss';
 import './App.css';
 import './App.scss';
 
-const App = () => (
-  <div>
-    Welcome <span className="blue">to</span> <span className={styles.red}>KKT</span>.
-    <br />
-  </div>
-);
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload for <span className={styles.red}>KKT</span>.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
 
 export default App;
