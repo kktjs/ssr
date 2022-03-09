af476d016f270ebca68e9ea434d4538f6ab59a6f
SunLxy <1011771396@qq.com>
2022-03-09 23:07:51 +0800

    question: @kkt/react-ssr-enhanced 剩余问题

--- packages/react-ssr-enhanced/src/index.ts
+++ packages/react-ssr-enhanced/src/index.ts
@@ -6,6 +6,12 @@ import ensureReady from './ensureReady';
 import render from './render';
 import { loadInitialProps } from './loadInitialProps';
 
+/** 问题
+ * 1. 页面的 store 状态调用问题
+ * 2. 懒加载页面问题
+ * 3. 懒加载 store
+ * */
+
 export {
   render,
   RoutersController,
