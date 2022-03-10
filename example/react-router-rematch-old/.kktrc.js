9c4b83d1ff197fe100c5559db98e0b5da501f5c4
SunLxy <1011771396@qq.com>
2022-03-10 16:30:35 +0800

    fix:修改 开发模式 下报错

--- example/react-router-rematch-old/.kktrc.js
+++ example/react-router-rematch-old/.kktrc.js
@@ -4,6 +4,19 @@ import webpack from "webpack"
 export default (conf, evn, options) => {
   if (!options.bundle) {
     conf = restWebpackManifestPlugin(conf);
+    // conf.resolve.fallback = {
+    //   "http": false,
+    //   "tty": false,
+    //   "https": false,
+    //   "zlib": false,
+    //   "tty": false,
+    //   "util": false,
+    //   "stream": false,
+    //   "assert": false,
+    //   "crypto": false,
+    //   "stream": false,
+    //   "os": false
+    // }
   }
   if (options.bundle) {
     // 暂时使用这种方式 解决 server 没有 window 问题
