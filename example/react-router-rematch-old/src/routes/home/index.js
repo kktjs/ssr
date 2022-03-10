89ba62503d156fdc0535141416094b3880592f49
SunLxy <1011771396@qq.com>
2022-03-10 10:56:03 +0800

    fix:暂时解决 window 变量问题

--- example/react-router-rematch-old/src/routes/home/index.js
+++ example/react-router-rematch-old/src/routes/home/index.js
@@ -14,12 +14,10 @@ class Home extends React.Component {
     if (req && store.dispatch.global && store.dispatch.global.verify && req.cookies) {
       token = req.cookies.token;
     }
-    // if (window && typeof window !== 'undefined') {
-    //   token = cookie.get('token');
-    // }
-    // store.dispatch.global.verify
-    // console.log("store", store.dispatch.global.verify({ token: 2121 }))
-    // store.dispatch.global.verify({ token })
+    if (window && typeof window !== 'undefined') {
+      token = cookie.get('token');
+    }
+    await store.dispatch.global.verify({ token: 2121 })
     return Promise.resolve({
       whatever: 'Home stuff', isServer: true
     })
