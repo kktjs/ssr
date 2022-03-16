54a392e3337e0b5b6c2902e036b4ff522a9db0f6
SunLxy <1011771396@qq.com>
2022-03-16 09:46:49 +0800

    script:修改案例命令

--- example/basic-routes-rematch-new/src/serverIndex.js
+++ example/basic-routes-rematch-new/src/serverIndex.js
@@ -9,7 +9,7 @@ import store from "./models"
 import App from './routes';
 
 // require 方式 打包报错
-const assetsMainifest = new Function(`return ${FS.readFileSync(`${OUTPUT_PUBLIC_PATH}/asset-manifest.json`, "utf-8")}`)()
+const assetsMainifest = new Function(`return ${FS.readFileSync(`${OUTPUT_PUBLIC_PATH}/asset-client-manifest.json`, "utf-8")}`)()
 
 const assets = {}
 
