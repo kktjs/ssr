c7d332a38ffe69c2a29bfbf424ed6b72969e2232
SunLxy <1011771396@qq.com>
2022-03-24 13:34:27 +0800

    style:修改案例配置

--- example/react-router-rematch-old/.kktssrrc.js
+++ example/react-router-rematch-old/.kktssrrc.js
@@ -4,38 +4,35 @@ export default {
   proxySetup: (app) => ({
     path: "./mocker/index.js",
   }),
-  overridesWebpack: (confArr, env, options) => {
-    const arr = []
-    confArr.forEach((conf,) => {
-      const newConfig = pluginLess(conf, {
-        target: conf.target,
-        env,
-        paths: options.paths
-      })
-      arr.push({
-        ...newConfig,
-        module: {
-          ...newConfig.module,
+  overridesClientWebpack: (conf, env, options) => {
+    return {
+      ...conf,
+      resolve: {
+        ...conf.resolve,
+        fallback: {
+          "util": require.resolve("util/"),
+          "crypto": require.resolve("crypto-browserify"),
+          "stream": require.resolve("stream-browserify"),
+          "http": require.resolve("stream-http"),
+          "https": require.resolve("https-browserify"),
+          "url": require.resolve("url/"),
+          "zlib": require.resolve("browserify-zlib"),
+          "tty": require.resolve("tty-browserify"),
+          "assert": require.resolve("assert/"),
+          "path": require.resolve("path-browserify"),
+          "os": require.resolve("os-browserify/browser"),
+          "buffer": require.resolve("buffer/")
         },
-        resolve: {
-          ...newConfig.resolve,
-          fallback: !/node/.test(newConfig.target) && {
-            "util": require.resolve("util/"),
-            "crypto": require.resolve("crypto-browserify"),
-            "stream": require.resolve("stream-browserify"),
-            "http": require.resolve("stream-http"),
-            "https": require.resolve("https-browserify"),
-            "url": require.resolve("url/"),
-            "zlib": require.resolve("browserify-zlib"),
-            "tty": require.resolve("tty-browserify"),
-            "assert": require.resolve("assert/"),
-            "path": require.resolve("path-browserify"),
-            "os": require.resolve("os-browserify/browser"),
-            "buffer": require.resolve("buffer/")
-          } || {},
-        }
-      })
+      }
+    }
+  },
+  overridesCommonWebpack: (conf, env, options) => {
+    const newConfig = pluginLess(conf, {
+      target: conf.target,
+      env,
+      paths: options.paths
     })
-    return arr
-  }
+    return newConfig
+  },
+
 }
