d9af6bb8b8a21b06aa11d5d190f9f9201c9a1fa8
SunLxy <1011771396@qq.com>
2022-03-30 15:13:36 +0800

    style:优化代码

--- core/src/utils/index.ts
+++ core/src/utils/index.ts
@@ -1,3 +1,24 @@
 export * from "./module"
 export * from "./plugins"
 export * from "./output"
+
+type BoolenValue = boolean | undefined | "false" | "true"
+
+export const getBoolean = (one: BoolenValue, two: BoolenValue, defaultValue: boolean = false) => {
+  let value = defaultValue
+  if (typeof one === "boolean") {
+    value = one
+  } else if (typeof two === "boolean") {
+    value = two
+  }
+
+  if ((typeof one === "string" && one === "true") || (typeof two === "string" && two === "true")) {
+    value = true
+  }
+
+  if ((typeof one === "string" && one === "false") || (typeof two === "string" && two === "false")) {
+    value = false
+  }
+
+  return value
+}
