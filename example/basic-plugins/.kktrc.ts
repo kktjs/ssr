8ffd0ad9a7a5946267c23a3c0ca9a57b88c530bc
SunLxy <1011771396@qq.com>
2022-04-07 17:33:50 +0800

    type(basic-plugins):修复类型问题

--- example/basic-plugins/.kktrc.ts
+++ example/basic-plugins/.kktrc.ts
@@ -3,9 +3,11 @@ import WebpackPluginSSRProps, { clearHtmlTemp, createNewWebpackManifestPlugin } 
 import { WebpackConfiguration } from "kkt"
 export default (conf: WebpackConfiguration, env: "development" | "production") => {
   const paths = require("react-scripts/config/paths")
-  conf.plugins.push(new WebpackPluginSSRProps());
-  conf.plugins.push(createNewWebpackManifestPlugin(paths, "client", env === "development"));
+  conf.plugins?.push(new WebpackPluginSSRProps());
+  conf.plugins?.push(createNewWebpackManifestPlugin(paths, "client", env === "development"));
   conf = clearHtmlTemp(conf)
-  conf.module.exprContextCritical = false;
+  if (conf.module) {
+    conf.module.exprContextCritical = false;
+  }
   return conf;
 };
