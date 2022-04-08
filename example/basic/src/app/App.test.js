c255f862b90884c96ec7ffb3fca24a59fecac058
SunLxy <1011771396@qq.com>
2022-04-08 09:17:01 +0800

    refactor(basic):用ts重新案例

--- example/basic/src/app/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-describe('<App />', () => {
-  test('renders without exploding', () => {
-    ReactDOM.render(<App />, document.createElement('div'));
-  });
-});
