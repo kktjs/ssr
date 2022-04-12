10f06f22c0dc687b8b796ce96706dbedb01aad41
SunLxy <1011771396@qq.com>
2022-04-12 09:59:29 +0800

    fix(@examples/basic-routes-rematch-old):修复状态管理

--- example/react-router-rematch-old/src/store/index.ts
+++ example/react-router-rematch-old/src/store/index.ts
@@ -1,47 +1,40 @@
-import { init, Models } from '@rematch/core';
+import { init, } from '@rematch/core';
 import cookie from 'cookiejs';
 import global from '../models/global';
 import stores, { Store, RootModel, FullModel } from '../models';
 
-let storeInit: any = {};
-
+let storeInit = init<RootModel, FullModel>({
+  models: {
+    global: global
+  },
+  plugins: [
+    {
+      createMiddleware: () => () => next => (action) => {
+        if (typeof window !== 'undefined') {
+          const token = cookie.get('token');
+          if (token) {
+            cookie.set('token', token, 1);
+          }
+        }
+        // do something here
+        return next(action);
+      },
+    },
+  ],
+});
 export const store = (): Store => storeInit;
 export const createStore = async (initialState = stores.getState() || {}) => {
   const promises: Promise<void>[] = [];
-
   Object.keys(initialState).forEach((name) => {
-    promises.push(import(`../models/${name}.js`).then((md) => {
-      const model = md.default || md;
-      model.state = initialState[name] || {};
-      model.name = name;
-      return model;
-    }));
+    if (name !== "loading") {
+      promises.push(import(`../models/${name}.ts`).then((md) => {
+        const model = md.default || md;
+        model.state = initialState[name] || {};
+        model.name = name;
+        return storeInit.addModel({ ...model });
+      }));
+    }
   });
-
-  const models = await Promise.all(promises);
-
-  let model: any = { global: global }
-
-  if (models.length > 0) {
-    model = models
-  }
-
-  storeInit = init<RootModel, FullModel>({
-    models: model,
-    plugins: [
-      {
-        createMiddleware: () => () => next => async (action) => {
-          if (typeof window !== 'undefined') {
-            const token = cookie.get('token');
-            if (token) {
-              await cookie.set('token', token, 1);
-            }
-          }
-          // do something here
-          return next(action);
-        },
-      },
-    ],
-  });
+  await Promise.all(promises);
   return stores;
 };
